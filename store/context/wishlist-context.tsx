fde6f089ab18586a6fe9b630960d8819235a2da1
Justuce Heninger <justuceh@gmail.com>
2023-06-10 21:32:30 -0700

    created wishlist context store and wrapped app with context provider

--- store/context/wishlist-context.tsx
+++ store/context/wishlist-context.tsx
@@ -1,13 +1,66 @@
-import { createContext } from 'react';
+import { createContext, useState } from 'react';
 
-const WishlistContext = createContext({
-	id: null,
+interface Game {
+	ids: number[] | null;
+	slug: string;
+	name: string;
+	description: string;
+	background_image: string;
+	releaseDate: string;
+	metacritic: number | null;
+	website: string;
+	redditUrl: string;
+	addWishlistItem: (id: number) => void;
+	removeWishlistItem: (id: number) => void;
+}
+
+const WishlistContext = createContext<Game>({
+	ids: null,
 	slug: '',
 	name: '',
 	description: '',
-	backgroundImageUrl: '',
+	background_image: '',
 	releaseDate: '',
 	metacritic: null,
 	website: '',
 	redditUrl: '',
+	addWishlistItem: (id: number) => {},
+	removeWishlistItem: (id: number) => {},
 });
+
+// actual type: { children: React.ReactNode }
+const WishlistContextProvider = ({ children }: any) => {
+	const [wishlistIds, setWishlistIds] = useState<number[]>([]);
+
+	const addWishlistItem = (id: number) => {
+		setWishlistIds((currentIds) => [...currentIds, id]);
+	};
+
+	const removeWishlistItem = (id: number) => {
+		setWishlistIds((currentIds) =>
+			currentIds.filter((wishlistId) => wishlistId !== id)
+		);
+	};
+
+	const contextValue: Game = {
+		ids: wishlistIds,
+		slug: '',
+		name: '',
+		description: '',
+		background_image: '',
+		releaseDate: '',
+		metacritic: null,
+		website: '',
+		redditUrl: '',
+		addWishlistItem,
+		removeWishlistItem,
+	};
+
+	return (
+		<WishlistContext.Provider value={contextValue}>
+			{children}
+		</WishlistContext.Provider>
+	);
+};
+
+export { WishlistContext, WishlistContextProvider };
