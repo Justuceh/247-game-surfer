9bd5e767fc8ecf08d4d3a1abc10fbd2b2b5705f8
Justuce Heninger <justuceh@gmail.com>
2023-06-12 01:50:43 -0700

    added game deal screen and added game store context

--- screens/GameDealsScreen.tsx
+++ screens/GameDealsScreen.tsx
@@ -1,28 +1,50 @@
+import { CHEAPSHARK_API_URL } from '@env';
+import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
 import { View, StyleSheet, Text } from 'react-native';
 
-import SearchInput from '../components/SearchInput';
+export interface GameDealsProps {
+	storeID: string;
+}
 
-const GameDealsScreen = () => {
-	function onGameDealSearchHandler() {
-		console.log('searched game deals');
+export interface GameDealItem {
+	gameID: string;
+	storeID: string;
+	title: string;
+	internalname: string;
+	normalPrice: string;
+	salePrice: 'string';
+	thumb: string;
+	isOnSale: '1' | '0';
+	dealRating: string;
+	dealID: string;
+}
+
+const GameDealsScreen = ({ storeID }: GameDealsProps) => {
+	async function fetchGameStoreDeals() {
+		const params = {
+			storeID: storeID,
+			pageSize: 20,
+		};
+		return await axios
+			.get(`${CHEAPSHARK_API_URL}/deals`, { params })
+			.then((response) => {
+				if (!response) {
+					throw new Error('Network response was not ok');
+				}
+				return response.data;
+			})
+			.catch((err) => err);
 	}
 
-	function handleOnChangeText(text: string) {
-		console.log(text);
-	}
+	const {
+		data: games,
+		isLoading,
+		error,
+	} = useQuery<GameDealItem[], unknown>(['gameDeals'], fetchGameStoreDeals);
+	const filteredGames = games?.filter((game) => game.dealID !== undefined);
 
-	return (
-		<View style={styles.rootContainer}>
-			<View style={styles.searchContainer}>
-				<SearchInput
-					onChangeText={handleOnChangeText}
-					onSearchHandler={onGameDealSearchHandler}
-				/>
-			</View>
-
-			<View style={styles.dealsContainer}></View>
-		</View>
-	);
+	return <View></View>;
 };
 
 export default GameDealsScreen;
@@ -30,12 +52,5 @@ export default GameDealsScreen;
 const styles = StyleSheet.create({
 	rootContainer: {
 		flex: 1,
-	},
-	searchContainer: {
-		flex: 1,
-	},
-	dealsContainer: {
-		flex: 9,
-		margin: 10,
 	},
 });
