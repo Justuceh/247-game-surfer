6e4decf582f40b5493cac2ce606f290b6cef71a0
Justuce Heninger <justuceh@gmail.com>
2023-06-24 15:01:27 -0700

    added static icon dimensions

--- components/GameDealCard.tsx
+++ components/GameDealCard.tsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useLayoutEffect, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { Text, View, StyleSheet, Pressable, Image } from 'react-native';
 
 import { GameStoreContext } from '../store/context/game_deals/game-stores-context';
@@ -25,14 +25,12 @@ const GameDealCard = ({
 	const storesContext = useContext(GameStoreContext);
 	const [gameImageHeight, setGameImageHeight] = useState<number>(0);
 	const [gameImageWidth, setGameImageWidth] = useState<number>(0);
-	const [storeIconImageHeight, setStoreIconImageHeight] = useState<number>(0);
-	const [storeIconImageWidth, setStoreIconImageWidth] = useState<number>(0);
 	const storeIcon = storesContext.stores.find(
 		(store) => store.storeID === gameDealItem.storeID
 	)?.images.icon;
 	const storeIconUri = `${CHEAPSHARK_BASE_URL}${storeIcon}`;
 
-	useLayoutEffect(() => {
+	useEffect(() => {
 		Image.getSize(
 			gameDealItem.thumb,
 			(width, height) => {
@@ -44,19 +42,6 @@ const GameDealCard = ({
 			}
 		);
 	}, [gameDealItem.thumb]);
-
-	useLayoutEffect(() => {
-		Image.getSize(
-			storeIconUri,
-			(width, height) => {
-				setStoreIconImageHeight(height);
-				setStoreIconImageWidth(width);
-			},
-			(error) => {
-				console.error('Failed to get image dimensions:', error);
-			}
-		);
-	}, [storeIcon]);
 
 	const calculatedWidth = gameImageWidth > 190 ? 190 : gameImageWidth + 50;
 	const calculatedHeight = gameImageHeight > 190 ? 120 : gameImageHeight + 60;
@@ -94,8 +79,8 @@ const GameDealCard = ({
 						</Text>
 						<Image
 							style={{
-								width: storeIconImageWidth,
-								height: storeIconImageHeight,
+								width: 16, // verified icon width dimensions
+								height: 16, // verified icon height dimensions
 								flex: 1,
 								padding: 1,
 								marginRight: 6,
