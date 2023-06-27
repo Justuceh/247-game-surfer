d27361c2006ad12e1234d3df7edeff5ed6ffc8d7
Justuce Heninger <justuceh@gmail.com>
2023-06-26 22:56:25 -0700

    added environment variables for igdb

--- env.d.ts
+++ env.d.ts
@@ -2,4 +2,8 @@ declare module '@env' {
 	export const CHEAPSHARK_BASE_URL: string;
 	export const CHEAPSHARK_API_URL: string;
 	export const CHEAPSHARK_REDIRECT_API: string;
+	export const IGDB_BASE_URL: string;
+	export const IGDB_IMAGE_URL: string;
+	export const IGDB_CLIENT_ID: string;
+	export const IGDB_SECRET: string;
 }
