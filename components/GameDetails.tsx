92b157c295c17b01cc91f8fc3892aa22a72a332b
Justuce Heninger <justuceh@gmail.com>
2023-06-29 00:17:26 -0700

    added logic to make api call to igdb games to games detail screen using the auth token context

--- components/GameDetails.tsx
+++ components/GameDetails.tsx
@@ -9,6 +9,7 @@ import { GameDealItem } from '../screens/GameDealsScreen';
 import { AuthContext } from '../store/context/auth/auth-context';
 import axios from 'axios';
 import { useQuery } from '@tanstack/react-query';
+import ActivityIndicatorComponent from './ActivityIndicator';
 
 interface GameDetailsProps {
 	gameDealItem: GameDealItem | undefined;
@@ -25,22 +26,30 @@ const GameDetails = ({
 	const headers = authContext.getRequestHeaders();
 
 	async function fetchGames() {
+		const query = `
+      fields *; 
+      search "${gameDealItem?.title}"; 
+      limit 1; 
+    `;
 		return await axios
-			.post(`${IGDB_BASE_URL}games`, {}, { headers: headers })
+			.post(`${IGDB_BASE_URL}games`, query, { headers: headers })
 			.then((response) => {
 				if (!response) {
 					throw new Error('Network response was not ok');
 				}
 				return response.data;
 			})
-			.catch((err) => err);
+			.catch((err) => console.log(err));
 	}
 
 	const {
-		data: games,
+		data: game,
 		isLoading,
 		refetch,
-	} = useQuery<any, unknown>([`games`], fetchGames);
+	} = useQuery<any, unknown>(
+		[`gameName-${gameDealItem?.title.replace(/\s+/g, '')}`],
+		fetchGames
+	);
 
 	const openBrowserAsync = async (dealID: string) => {
 		await WebBrowser.openBrowserAsync(`${CHEAPSHARK_REDIRECT_API}${dealID}`);
@@ -48,7 +57,11 @@ const GameDetails = ({
 	return (
 		<ModalComponent onClose={onClose} visible={showDetails}>
 			<View style={styles.rootContainer}>
-				<Text style={{ color: 'white' }}>{gameDealItem?.title}</Text>
+				{isLoading ? (
+					<ActivityIndicatorComponent color='white' size='large' />
+				) : (
+					<Text style={{ color: 'white' }}>{game?.[0]?.name}</Text>
+				)}
 			</View>
 		</ModalComponent>
 	);
